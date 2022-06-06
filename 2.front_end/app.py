5aefe48625037fd509307cdda1bd44508065abec
Neo Sebanze <neo@bidvestalice.com>
2022-06-06 19:21:29 +0200

    ADDED IMAGE AND DATA

--- 2.front_end/app.py
+++ 2.front_end/app.py
@@ -4,10 +4,11 @@ import json
 app = Flask(__name__)
 
 #GET DATA FROM JSON
-#def get_rankedcoins():
-# file= open('back_end/sample.json')
-# data = json.load(file)
+# def get_rankedcoins():
+file= open('backend_data/sample.json')
+data = json.load(file)
 
 @app.route("/")
 def home():
-     return render_template('index.html')
+     return render_template('index.html',data = data)
+ 
