ec8904f238a5a3e2f5b8666797c38b80986f8cf3
Neo Sebanze <neo@bidvestalice.com>
2022-06-06 16:20:42 +0200

    CREATED QR CODE

--- 1.back_end/a_qrscanner.py
+++ 1.back_end/a_qrscanner.py
@@ -1,3 +1,4 @@
+import qrcode
 
 #1.Obtain data
 name= input("What is your name?")
@@ -6,4 +7,7 @@ last_location=input("What is your last location")
 
 #2.Store Data Dictionary
 user_data= {"name": name, "first_location":first_location,"last_location":last_location}
-print(user_data)
+
+#CREATE QR CODE
+img=qrcode.make(user_data)
+img.save("pic.png")
