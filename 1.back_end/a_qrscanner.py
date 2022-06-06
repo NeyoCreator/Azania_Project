eb03f1ef8e896b0782ccdb024b5a84cd9eec01ec
Neo Sebanze <neo@bidvestalice.com>
2022-06-06 16:12:39 +0200

    OBTAINED DATA

---
+++ 1.back_end/a_qrscanner.py
@@ -0,0 +1,9 @@
+
+#1.Obtain data
+name= input("What is your name?")
+first_location= input("What is your first location?")
+last_location=input("What is your last location")
+
+#2.Store Data Dictionary
+user_data= {"name": name, "first_location":first_location,"last_location":last_location}
+print(user_data)
