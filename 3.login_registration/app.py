c936bb110e34e4929989904256ba99e1049769d7
Neo Sebanze <neo@bidvestalice.com>
2022-06-07 14:51:16 +0200

    BOOTSTRAPED SIGN UP PAGE

--- 3.login_registration/app.py
+++ 3.login_registration/app.py
@@ -1,6 +1,44 @@
-from flask import Flask
-app = Flask(__name__)
+from email import message
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,BooleanField
+from wtforms.validators import InputRequired, Email, Length
+import email_validator
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='RThsiissecrete!'
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username',validators = [InputRequired(), Length(min=4, max=15 )])
+    password = PasswordField('password', validators = [InputRequired(),Length(min=8, max=80)])
+    remember = BooleanField('remember')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators= [InputRequired(),Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username',validators= [InputRequired(), Length(min=4, max=15 )])
+    password = PasswordField('password', validators= [InputRequired(),Length(min=8, max=80)])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',form=form)
+
+@app.route('/signup')
+def signup():
+    form = RegisterForm()
+    return render_template('signup.html',form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
