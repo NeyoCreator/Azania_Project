51f088aaf75eef9a7ba76e7eea06f0af6c8e1803
Neo Sebanze <neo@bidvestalice.com>
2022-06-07 19:34:25 +0200

    MANAGED TO CREATE USER AND LOGIN

--- 3.login_registration/app.py
+++ 3.login_registration/app.py
@@ -1,6 +1,6 @@
 
 from email import message
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,url_for 
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,BooleanField
@@ -33,7 +33,6 @@ class RegisterForm(FlaskForm):
     username = StringField('username',validators= [InputRequired(), Length(min=4, max=15 )])
     password = PasswordField('password', validators= [InputRequired(),Length(min=8, max=80)])
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -42,7 +41,13 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user = User.query.filter_by(username = form.username.data).first()  
+        if user :
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+                
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template('login.html',form=form)
 
