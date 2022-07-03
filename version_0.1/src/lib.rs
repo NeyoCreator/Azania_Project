a9c5ed099fcb5637b8563ace750d40fb763aa22d
Neo Sebanze <neo@bidvestalice.com>
2022-07-03 03:18:44 +0200

    CHANGED GUMBALL MACHINE TO DINAR

--- version_0.1/src/lib.rs
+++ version_0.1/src/lib.rs
@@ -1,49 +1,49 @@
 use scrypto::prelude::*;
 
 blueprint! {
-    struct GumballMachine {
-        // Vault to store the remaining gumballs
-        gumballs: Vault,
+    struct TokeMachine {
+        // Vault to store the remaining dinars
+        dinar_vault: Vault,
         // Vault to store the XRD payments
         collected_xrd: Vault,
-        // Represents the price to buy a gumball
+        // Represents the price to buy a dinars
         price: Decimal
     }
 
-    impl GumballMachine {
+    impl TokeMachine {
         pub fn instantiate_machine(price: Decimal) -> ComponentAddress {
-            // Create a new gumball resource
-            let gumballs: Bucket = ResourceBuilder::new_fungible()
+            // Create a new dinar resource
+            let dinar_vault: Bucket = ResourceBuilder::new_fungible()
                 .divisibility(DIVISIBILITY_NONE)
-                .metadata("name", "Gumball")
-                .metadata("symbol", "GUM")
-                .initial_supply(300);
+                .metadata("name", "Dinars")
+                .metadata("symbol", "DNH")
+                .initial_supply(500);
 
             // Instantiate a new component and return it
             Self {
-                gumballs: Vault::with_bucket(gumballs),
+                dinar_vault: Vault::with_bucket(dinar_vault),
                 collected_xrd: Vault::new(RADIX_TOKEN),
                 price: price
             }
             .instantiate().globalize()
         }
 
-        // Allow users to buy a gumball by providing enough XRD.
-        // Returns a single gumball and the remaining of the payment bucket (the change)
-        pub fn buy_gumball(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
-            info!("Buying a gumball!");
+        // Allow users to buy a dinar by providing enough XRD.
+        // Returns a single dinar and the remaining of the payment bucket (the change)
+        pub fn buy_dinar(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
+            info!("Buying a dinar!");
             // Take a portion of the payment bucket depending on the price
             let our_share = payment.take(self.price);
 
             // Insert the portion of XRD inside the collected_xrd vault
             self.collected_xrd.put(our_share);
 
-            // Take a single gumball
-            let gumball: Bucket = self.gumballs.take(1);
+            // Take a single dinar
+            let dinar: Bucket = self.dinar_vault.take(1);
 
-            // Return the gumball and the XRD remaining in 
+            // Return the dinar and the XRD remaining in 
             // the payment bucket (if the user sent too much)
-            (gumball, payment)
+            (dinar, payment)
         }
     }
 }
