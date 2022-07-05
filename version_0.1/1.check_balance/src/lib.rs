bdbbbbbdeb175be9331401956bc22a73f7366a98
Neo Sebanze <neo@bidvestalice.com>
2022-07-05 20:27:37 +0200

    CREATED A BALANCE CHECKER COMPONENT

--- version_0.1/1.check_balance/src/lib.rs
+++ version_0.1/1.check_balance/src/lib.rs
@@ -1,48 +1,33 @@
 use scrypto::prelude::*;
 
 blueprint! {
-    struct TokenMachine {
-        // Vault to store the remaining dinars
-        dinar_vault: Vault,
-        // Vault to store the XRD payments
-        collected_xrd: Vault,
-        // Represents the price to buy a dinars
-        price: Decimal
+    struct UserVault {
+        //0.DEFINE RESOURCE
+        user_vault: Vault
+
     }
 
-    impl TokenMachine {
-        pub fn instantiate_machine(price: Decimal) -> ComponentAddress {
-            // Create a new dinar resource
-            let dinar_vault: Bucket = ResourceBuilder::new_fungible()
-                .divisibility(DIVISIBILITY_NONE)
-                .metadata("name", "Dinars")
-                .metadata("symbol", "DNH")
-                .initial_supply(500);
-            // Instantiate a new component and return it
-            Self {
-                dinar_vault: Vault::with_bucket(dinar_vault),
-                collected_xrd: Vault::new(RADIX_TOKEN),
-                price: price
-            }
-            .instantiate().globalize()
+    impl UserVault {
+        //1.INSTANTIATE
+        pub fn instatiate_dnr() -> ComponentAddress{
+            //1.1.CREATE TOKEN
+            let my_bucket: Bucket = ResourceBuilder::new_fungible()
+                .metadata("name", "DinarToken")
+                .metadata("symbol", "DNR")
+                .initial_supply(100);
+            //1.2.INSTANTIATE AND POPULATING RESOURCE
+            Self{
+                user_vault: Vault::with_bucket(my_bucket)
+            }.instantiate().globalize()
+        
         }
 
-        // Allow users to buy a dinar by providing enough XRD.
-        // Returns a single dinar and the remaining of the payment bucket (the change)
-        pub fn buy_dinar(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
-            info!("Buying a dinar!");
-            // Take a portion of the payment bucket depending on the price
-            let our_share = payment.take(self.price);
-
-            // Insert the portion of XRD inside the collected_xrd vault
-            self.collected_xrd.put(our_share);
-
-            // Take a single dinar
-            let dinar: Bucket = self.dinar_vault.take(1);
-
-            // Return the dinar and the XRD remaining in 
-            // the payment bucket (if the user sent too much)
-            (dinar, payment)
+        //2.MEHTOD
+        pub fn check_balance(&mut self) -> Bucket{
+            info!("My balance is: {}", self.user_vault.amount());
+            self.user_vault.take(0)
         }
+
+
     }
 }
